688c928b069692897b13b4922e842ca742a00de6
ekra024 <ekra.cse8.bu@gmail.com>
2026-02-16 11:15:28 +0600

    bubbleSort

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -1,1 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(vector<int>&nums) {
+    for(int i = 0; i < nums.size(); i++) {
+        for(int j = 1; j < nums.size()-i; j++) {
+            if(nums[j-1] > nums[j]) {
+                swap(nums[j-1], nums[j]);
+            }
+        }
+    }
+}
+
+void Print(vector<int>nums) {
+    for(int i = 0; i < nums.size(); i++)
+        cout<< nums[i]<<' ';
+    cout<<'\n';
+}
+
+int main()
+{
+    int n; cin >> n;
+    vector<int>nums(n);
+
+    for(int i = 0; i < n; i++) cin >> nums[i];
+
+    bubbleSort(nums);
+    Print(nums);
+}
 
